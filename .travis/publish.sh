ccff0944363e2bb07bec632dca31f55fc898ee24
Radu Pisoi <radupisoi23@gmail.com>
2016-11-20 15:31:03 +0200

    Configure Travis CI to build oXygen user manual

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -57,17 +57,17 @@ git clone https://github.com/oxygenxml/userguide.git oXygen-ug
 echo "====================================="
 echo "Transform the oXygen user manual to PDF, editor distribution"
 echo "====================================="
-sh dita-ot-2.4/bin/dita -i oXygen-ug/DITA/UserManual.ditamap -f oxy-ug-pdf -output=publishing/oXygen-ug/editor -Doxy-ug-distribution=editor
+sh dita-ot-2.4/bin/dita -i oXygen-ug/DITA/UserManual.ditamap -f oxy-ug-pdf --filter=oXygen-ug/DITA/editor.ditaval -output=publishing/oXygen-ug/editor -Doxy-ug-distribution=editor
 
 echo "====================================="
 echo "Transform the oXygen user manual to PDF, author distribution"
 echo "====================================="
-sh dita-ot-2.4/bin/dita -i oXygen-ug/DITA/UserManual.ditamap -f oxy-ug-pdf -output=publishing/oXygen-ug/author -Doxy-ug-distribution=author
+sh dita-ot-2.4/bin/dita -i oXygen-ug/DITA/UserManual.ditamap -f oxy-ug-pdf -output=publishing/oXygen-ug/author --filter=oXygen-ug/DITA/author.ditaval -Doxy-ug-distribution=author
 
 echo "====================================="
 echo "Transform the oXygen user manual to PDF, developer distribution"
 echo "====================================="
-sh dita-ot-2.4/bin/dita -i oXygen-ug/DITA/UserManual.ditamap -f oxy-ug-pdf -output=publishing/oXygen-ug/developer -Doxy-ug-distribution=developer
+sh dita-ot-2.4/bin/dita -i oXygen-ug/DITA/UserManual.ditamap -f oxy-ug-pdf --filter=oXygen-ug/DITA/developer.ditaval -output=publishing/oXygen-ug/developer -Doxy-ug-distribution=developer
 
 cp gh-pages/index.html publishing/index.html
 echo "====================================="
